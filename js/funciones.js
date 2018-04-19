0664af1fe8f89dfb4f508d52fa0925d9619117ab
Jose <escalantep98@gmail.com>
2018-04-19 06:52:19 -0500

    previo

---
+++ js/funciones.js
@@ -0,0 +1,89 @@
+﻿$("#inicio").click(function(event){
+   $("#contenedor").load('paginas/principal.html');
+});
+
+$("#contacto").click(function(event){
+   $("#contenedor").load('paginas/contacto.html');
+});
+
+$("#registro").click(function(event){
+   $("#contenedor").load('paginas/registro.html');
+});
+
+$("#nosotros").click(function(event){
+   $("#contenedor").load('paginas/nosotros.html');
+});
+
+$("#estamos").click(function(event){
+   $("#contenedor").load('paginas/estamos.html');
+});
+
+$("#login").click(function(event){
+   $("#contenedor").load('paginas/login.html');
+});
+
+
+/*Funciones Contacto*/
+
+var inputs = document.getElementsByClassName('formulario__input');
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function(){
+    if(this.value.length>=1) {
+      this.nextElementSibling.classList.add('fijar');
+    } else {
+      this.nextElementSibling.classList.remove('fijar');
+    }
+  });
+}
+
+
+/*Funciones login*/
+
+$( document ).ready(function() {
+ 
+    loadProfile();
+});
+
+
+function getLocalProfile(callback){
+    var profileImgSrc      = localStorage.getItem("PROFILE_IMG_SRC");
+    var profileName        = localStorage.getItem("PROFILE_NAME");
+    var profileReAuthEmail = localStorage.getItem("PROFILE_REAUTH_EMAIL");
+
+    if(profileName !== null
+            && profileReAuthEmail !== null
+            && profileImgSrc !== null) {
+        callback(profileImgSrc, profileName, profileReAuthEmail);
+    }
+}
+
+
+function loadProfile() {
+    if(!supportsHTML5Storage()) { return false; }
+    
+    getLocalProfile(function(profileImgSrc, profileName, profileReAuthEmail) {
+        //changes in the UI
+        $("#profile-img").attr("src",profileImgSrc);
+        $("#profile-name").html(profileName);
+        $("#reauth-email").html(profileReAuthEmail);
+        $("#inputEmail").hide();
+        $("#remember").hide();
+    });
+}
+
+
+function supportsHTML5Storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+
+function testLocalStorageData() {
+    if(!supportsHTML5Storage()) { return false; }
+    localStorage.setItem("PROFILE_IMG_SRC", "//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" );
+    localStorage.setItem("PROFILE_NAME", "César Izquierdo Tello");
+    localStorage.setItem("PROFILE_REAUTH_EMAIL", "oneaccount@gmail.com");
+  }
